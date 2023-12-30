884769f7fa072e76de5f86a2f35a2605fd83e443
Henrique <hlopessantos52@gmail.com>
2023-12-30 20:09:41 -0300

    Pokemons working

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,41 @@
 <script setup>
+import { onMounted, reactive, ref } from "vue";
+import ListPokemons from "./ListPokemons.vue";
+
+let pokemons = reactive(ref());
+let urlBaseSvg = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/";
+
+onMounted(()=>{
+  fetch("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0")
+      .then(response => response.json())
+      .then(response => pokemons.value = response.results);
+})
 
 </script>
 
 <template>
-  <div class="container text-center">
-    <div class="row">
-      <div class="col">
-        Column
-      </div>
-      <div class="col">
-        Column
-      </div>
-      <div class="col">
-        Column
+  <div class="container">
+    <div class="row mt-4">
+<!--      <div class="col-sm-12 col-md-6">-->
+<!--        <div class="card" style="width: 18rem;">-->
+<!--          <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/151.png" class="card-img-top" alt="Mew">-->
+<!--          <div class="card-body">-->
+<!--            <h5 class="card-title">Card title</h5>-->
+<!--            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>-->
+<!--          </div>-->
+<!--        </div>-->
+<!--      </div>-->
+      <div class="col-sm-12 col-md-6">
+        <div class="card">
+          <div class="card-body">
+            <ListPokemons
+              v-for="pokemon in pokemons"
+              :key="pokemon.name"
+              :name="pokemon.name"
+              :urlBaseSvg="urlBaseSvg + pokemon.url.split('/')[6] + '.svg'"
+            />
+          </div>
+        </div>
       </div>
     </div>
   </div>
