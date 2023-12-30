db2c8d8ad51e79dd49fb4489ad87718fde01a246
Henrique <hlopessantos52@gmail.com>
2023-12-30 20:46:53 -0300

    Adjust View pokemon in screen

---
+++ src/components/CardPokemonSelected.vue
@@ -0,0 +1,27 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="card">
+    <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/151.png" class="card-img-top" alt="Mew">
+    <div class="card-body">
+      <h5 class="card-title text-center">Card title</h5>
+      <hr>
+      <div class="row">
+        <section class="col">
+          <strong>XP:</strong>
+          <span>50</span>
+        </section>
+        <section class="col">
+          <strong>XP:</strong>
+          <span>50</span>
+        </section>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { onMounted, reactive, ref, computed } from "vue";
 import ListPokemons from "./ListPokemons.vue";
+import CardPokemonSelected from "@/components/CardPokemonSelected.vue";
 
 let pokemons = reactive(ref());
 let urlBaseSvg = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/";
@@ -26,13 +27,7 @@ const filteredPokemons = computed(()=>{
     <div class="container">
       <div class="row mt-4">
         <div class="col-sm-12 col-md-6">
-          <div class="card" style="width: 18rem;">
-            <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/151.png" class="card-img-top" alt="Mew">
-            <div class="card-body">
-              <h5 class="card-title">Card title</h5>
-              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-            </div>
-          </div>
+          <card-pokemon-selected/>
         </div>
         <div class="col-sm-12 col-md-6">
           <div class="card">
