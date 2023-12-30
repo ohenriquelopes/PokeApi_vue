34d7bd55b9e1f0a58ca8aa7ca5172f760f2e49d1
Henrique <hlopessantos52@gmail.com>
2023-12-30 20:37:31 -0300

    FilteredPokemons

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,10 @@
 <script setup>
-import { onMounted, reactive, ref } from "vue";
+import { onMounted, reactive, ref, computed } from "vue";
 import ListPokemons from "./ListPokemons.vue";
 
 let pokemons = reactive(ref());
 let urlBaseSvg = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/";
+let searchPokemonField = ref();
 
 onMounted(()=>{
   fetch("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0")
@@ -11,34 +12,48 @@ onMounted(()=>{
       .then(response => pokemons.value = response.results);
 })
 
+const filteredPokemons = computed(()=>{
+  if(pokemons.value && searchPokemonField.value){
+    return pokemons.value.filter(pokemon => pokemon.name.includes(searchPokemonField.value.toLowerCase()))
+  }
+  return pokemons.value;
+})
+
 </script>
 
 <template>
-  <div class="container">
-    <div class="row mt-4">
-<!--      <div class="col-sm-12 col-md-6">-->
-<!--        <div class="card" style="width: 18rem;">-->
-<!--          <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/151.png" class="card-img-top" alt="Mew">-->
-<!--          <div class="card-body">-->
-<!--            <h5 class="card-title">Card title</h5>-->
-<!--            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>-->
-<!--          </div>-->
-<!--        </div>-->
-<!--      </div>-->
-      <div class="col-sm-12 col-md-6">
-        <div class="card">
-          <div class="card-body">
-            <ListPokemons
-              v-for="pokemon in pokemons"
-              :key="pokemon.name"
-              :name="pokemon.name"
-              :urlBaseSvg="urlBaseSvg + pokemon.url.split('/')[6] + '.svg'"
-            />
+  <main>
+    <div class="container">
+      <div class="row mt-4">
+        <div class="col-sm-12 col-md-6">
+          <div class="card" style="width: 18rem;">
+            <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/151.png" class="card-img-top" alt="Mew">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-12 col-md-6">
+          <div class="card">
+            <div class="card-body row">
+              <div class="mb-3">
+                <label hidden for="searchPokemonField" class="form-label">Pesquisar...</label>
+                <input v-model:="searchPokemonField" type="text" class="form-control" id="searchPokemonField" placeholder="Pokemons">
+              </div>
+
+              <ListPokemons
+                v-for="pokemon in filteredPokemons"
+                :key="pokemon.name"
+                :name="pokemon.name"
+                :urlBaseSvg="urlBaseSvg + pokemon.url.split('/')[6] + '.svg'"
+              />
+            </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
+  </main>
 </template>
 
 <style scoped>
--- src/views/ListPokemons.vue
+++ src/views/ListPokemons.vue
@@ -5,7 +5,7 @@
 
 <template>
   <div class="col-4">
-    <div class="card p-2 mb-3">
+    <div class="card p-2 mb-3 cardListPokemon">
       <p class="text-center">{{pokemon.name}}</p>
       <img :src="urlBaseSvg" class="card-img-top" alt="" height="80">
     </div>
@@ -13,5 +13,19 @@
 </template>
 
 <style scoped>
+  .cardListPokemon{
+    background: rgb(2,0,36);
+    background: radial-gradient(circle, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 35%, rgba(0,212,255,1) 100%);
+    cursor: pointer;
+  }
 
+  .cardListPokemon:hover{
+    background: rgb(2,0,36);
+    background: radial-gradient(circle, rgba(2,0,36,1) 0%, rgba(9,9,121,0.7) 35%, rgba(0,212,255,0.7) 100%);
+
+  }
+
+  .cardListPokemon img:hover{
+    padding: .2rem;
+  }
 </style>
